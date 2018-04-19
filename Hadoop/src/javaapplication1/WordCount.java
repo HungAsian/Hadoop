bd9f40592e92b8ff4f98df167bb45d476b5b93fc
HungAsian <hql0510@louisiana.edu>
2018-04-19 18:24:24 -0500

    Add files via upload

---
+++ Hadoop/src/javaapplication1/WordCount.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCount {
+
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, Text> {
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text guild = new Text();
+        private Text name = new Text();
+
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+
+            if (itr.countTokens() >= 2) {
+                name.set(itr.nextToken());
+                guild.set(itr.nextToken());
+
+                if(guild.toString().compareTo("N/A") != 0){
+                    context.write(guild, name);
+                }
+                
+                
+            }
+            //while (itr.hasMoreTokens()) {
+            //word.set(itr.nextToken());
+            //context.write(word, one);
+            //}
+        }
+    }
+
+    public static class IntSumReducer extends Reducer<Text, Text, Text, Text> {
+
+        private Text result = new Text();
+
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {/*
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+            
+             */
+            int cnt = 0;
+            for (Text val : values) {
+                if(cnt == 0)
+                    result.set(val.toString());
+                else
+                    result.set(result.toString() + ", " + val.toString());
+                cnt++;
+            }
+            context.write(key, result);
+            //result.clear();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
